a896d2cb0b7bd5ab1242fdab75aa874b6159d51d
Scott Rippey <scott.william.rippey@gmail.com>
2014-11-10 11:59:57 -0800

    [template-scope] Initial implementation of template-scope and template-scope-set directives (copied from plnkr)

---
+++ src/template-scope-directive.js
@@ -0,0 +1,77 @@
+angular.module('templateScope', [])
+	.directive('templateScopeSet', function () {
+		return {
+			controller: 'templateScopeController',
+			link: function (scope, element, attrs, setCtrl) {
+				setCtrl.setBindings(scope, attrs);
+			}
+		};
+	})
+	.directive('templateScope', function () {
+		return {
+			scope: true, // TODO: Bind to an isolate scope (instead of this "new" scope)
+			require: '^templateScopeSet',
+			link: function (scope, element, attrs, templateScopeController) {
+				var definitions = scope.$eval(attrs.templateScope);
+				templateScopeController.getBindings(scope, definitions);
+			}
+		};
+	})
+	.controller('templateScopeController', function TemplateScopeController() {
+		this.setBindings = setBindings;
+		this.getBindings = getBindings;
+
+		var parentScope, parentAttrs, templateScope, templateDefinitions;
+		function setBindings(scope, attrs) {
+			parentScope = scope;
+			parentAttrs = attrs;
+
+			_applyBindings();
+		}
+		function getBindings(scope, definitions) {
+			templateScope = scope;
+			templateDefinitions = definitions;
+		}
+
+		function _applyBindings() {
+			var definitions = templateDefinitions;
+			var scope = templateScope;
+
+			// Do all the hard stuff:
+			Object.keys(definitions).forEach(function (definition) {
+
+				var binding = definitions[definition];
+				var firstChar = binding.substr(0, 1);
+				var alias = binding.substr(1) || definition;
+
+				switch (firstChar) {
+					case "@":
+						// Input attribute
+						parentAttrs.$observe(alias, function (value) {
+							scope[definition] = value;
+						});
+						break;
+					case "&":
+						// Output parameter
+						scope[definition] = function (params) {
+							parentScope.$eval(parentAttrs[alias], params);
+						};
+						break;
+					case "=":
+						// Two-way databinding
+						parentScope.$watch(parentAttrs[alias], function (value) {
+							scope[definition] = value;
+						});
+						scope.$watch(definition, function (value) {
+							parentScope.$eval(parentAttrs[alias] + " = $newValue", { $newValue: value });
+						});
+						break;
+					default:
+						throw new Error("templateScope - Unrecognized binding: " + binding);
+				}
+
+			});
+		}
+
+	})
+;
